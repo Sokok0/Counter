1c5884c5299d709a3284e1c4f53178d08183986a
Sokok0 <148645480+Sokok0@users.noreply.github.com>
2025-10-30 14:26:36 +0100

    Update CounterView.xaml.cs

--- Counter/Counter/Views/CounterView.xaml.cs
+++ Counter/Counter/Views/CounterView.xaml.cs
@@ -1,14 +1,19 @@
-﻿namespace Counter
+using System.Collections.ObjectModel; //potrzebne do listy counters
+
+namespace Counter
 {
     public partial class CounterView : ContentView
     {
         public CounterViewModel Model { get; private set; }
         public event EventHandler? Removed;
 
-        public CounterView(CounterViewModel model)
+        private ObservableCollection<CounterViewModel> countersRef; //referencja do listy liczników
+
+        public CounterView(CounterViewModel model, ObservableCollection<CounterViewModel> counters) //nowy konstruktor
         {
             InitializeComponent();
             Model = model;
+            countersRef = counters; //zapisujemy referencję
             UpdateUI();
         }
 
@@ -23,18 +28,38 @@
         {
             Model.Value++;
             UpdateUI();
+            CounterStorage.Save(countersRef); //zapis po zmianie
         }
 
         private void DecreaseClicked(object sender, EventArgs e)
         {
             Model.Value--;
             UpdateUI();
+            CounterStorage.Save(countersRef); //zapis po zmianie
         }
 
         private void ResetClicked(object sender, EventArgs e)
         {
             Model.Value = Model.InitialValue;
             UpdateUI();
+            CounterStorage.Save(countersRef); //zapis po zmianie
+        }
+
+        private async void RenameClicked(object sender, EventArgs e) //nowa metoda Rename
+        {
+            string result = await Application.Current.MainPage.DisplayPromptAsync(
+                "Rename Counter",
+                "Enter new name:",
+                initialValue: Model.Name,
+                maxLength: 50,
+                keyboard: Keyboard.Text);
+
+            if (!string.IsNullOrWhiteSpace(result))
+            {
+                Model.Name = result.Trim();
+                UpdateUI();
+                CounterStorage.Save(countersRef); //zapis po zmianie nazwy
+            }
         }
 
         private void RemoveClicked(object sender, EventArgs e)
